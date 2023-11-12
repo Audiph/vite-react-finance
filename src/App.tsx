c69c38de4940de4ad03225809f5a07617569d82f
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-11-12 14:45:15 +0800

    integrate MUI theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { useMemo } from 'react';
+import { themeSettings } from './theme';
+import { createTheme } from '@mui/material/styles';
+
 function App() {
-  return <div className="app"></div>;
+  const theme = useMemo(() => createTheme(themeSettings), []);
+
+  return (
+    <div className="app">
+      <ThemeProvider theme={theme}>
+        {/* reset css settings to default that MUI has set */}
+        <CssBaseline />
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
