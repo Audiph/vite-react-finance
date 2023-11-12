0bb49de32f28566643e670a79eab99ffcc62acbc
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-11-12 14:52:29 +0800

    setup routes and pages base element

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
-import { ThemeProvider, CssBaseline } from '@mui/material';
+import { ThemeProvider, CssBaseline, Box } from '@mui/material';
 import { useMemo } from 'react';
 import { themeSettings } from './theme';
 import { createTheme } from '@mui/material/styles';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
 
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        {/* reset css settings to default that MUI has set */}
-        <CssBaseline />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          {/* reset css settings to default that MUI has set */}
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Routes>
+              <Route path="/" element={<div>dashboard page</div>} />
+              <Route path="/predictions" element={<div>prediction page</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
