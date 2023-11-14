81a9bb37d4fdd0f5843eacbaf621fa74629f581f
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-11-14 17:51:20 +0800

    fix deployment issues

--- src/scenes/dashboard/Row3.tsx
+++ src/scenes/dashboard/Row3.tsx
@@ -163,7 +163,7 @@ const Row3 = () => {
                   paddingAngle={2}
                   dataKey="value"
                 >
-                  {data.map((entry, index) => (
+                  {data.map((_, index) => (
                     <Cell key={`cell-${index}`} fill={pieColors[index]} />
                   ))}
                 </Pie>
